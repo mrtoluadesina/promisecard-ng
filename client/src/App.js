9089977abcd389f000a67317ce914572f30b91aa
Tolu Adesina <tadesina90@gmail.com>
2019-12-08 00:43:24 +0100

    map current views to the App route

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import './App.scss';
+
+const Home = lazy(() => import('./views/Home'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Suspense fallback={<div>Loading ...</div>}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Redirect to="/" />
+          </Switch>
+        </Suspense>
+      </Router>
+    </>
   );
 }
 
