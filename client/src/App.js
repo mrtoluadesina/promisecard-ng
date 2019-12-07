55d9a60bc2e5dca741351e44c976494f0106b6de
Tolu Adesina <tadesina90@gmail.com>
2019-12-08 00:45:59 +0100

    remove unused imports and modularized routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,15 @@
-import React, { Suspense, lazy } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
-
-const Home = lazy(() => import('./views/Home'));
+import Routes from './routes';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Suspense fallback={<div>Loading ...</div>}>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Redirect to="/" />
-          </Switch>
-        </Suspense>
-      </Router>
-    </>
+    <Router>
+      <Suspense fallback={<div>Loading ...</div>}>
+        <Routes />
+      </Suspense>
+    </Router>
   );
 }
 
