d0f7a1e7a10a6fc02761eb9732c397de6e179cee
Tolu Adesina <tadesina90@gmail.com>
2019-12-09 07:37:19 +0100

    update the header component with logo and nav

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,1 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.scss';
+
+function Header(props) {
+  return (
+    <header>
+      <div className="logo">
+        <img
+          src={process.env.PUBLIC_URL + '/assets/images/logo.svg'}
+          alt="promise card ng"
+        />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
