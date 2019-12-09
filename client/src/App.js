ede2088f1c1d9d700f307aef8b686916020ae17f
Tolu Adesina <tadesina90@gmail.com>
2019-12-09 07:37:50 +0100

    add Header to the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React, { Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 import './App.scss';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Suspense fallback={<div>Loading ...</div>}>
         <Routes />
       </Suspense>
