0fa9da1ed202d3e2e03bef8d0b3e15eaff53db63
Tolu Adesina <tadesina90@gmail.com>
2019-12-10 09:51:41 +0100

    update the home page with necesssary markup

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -6,6 +6,9 @@ function Home(props) {
     <section className="main">
       <div className="app">
         <h2 className="title">Create your Promise Card!</h2>
+        <div className="cta-adv">
+          <h3>Who is gifting me something this christmas?</h3>
+        </div>
       </div>
     </section>
   );
