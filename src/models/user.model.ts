144cd847c8f8987e5d11fb60e71f0bddb46b7bae
whitehox <oketegah@gmail.com>
2019-12-16 02:11:08 +0100

    refactored edit model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,28 +1,14 @@
 import mongoose, { Document } from 'mongoose';
-import { hash } from 'bcrypt';
 
 interface UserType extends Document {
-  password: string;
+  name: string;
+  email: string;
 }
 
 const UserSchema = new mongoose.Schema({
   name: { type: String },
   email: { type: String, required: true },
-  phone: { type: String },
-  password: { type: String, required: true },
-});
-
-UserSchema.pre<UserType>('save', function(next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  hash(this.password, 12, (err, hash) => {
-    if (err) {
-      return next(err);
-    }
-    this.password = hash;
-    return next();
-  });
+  password: { type: String },
 });
 
 export default mongoose.model<UserType>('User', UserSchema);
